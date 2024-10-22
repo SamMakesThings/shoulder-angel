93d8030824cd56d7812de7430e2ab68f90e7dcdf
Sam Stowers <phantom4d@gmail.com>
2024-10-22 16:56:11 -0700

    Trace state functions

--- server/src/state.py
+++ server/src/state.py
@@ -1,18 +1,19 @@
 from typing import List
 import pickle
 from src.types import LLMMessage, GroqMessage
+import weave
 
 
 convo_history: List[dict] = []
 convo_history_file_path = "convo_history.pkl"
 
-
+@weave.op()
 def save_convo():
     """Save convo_history to a pickled file"""
     with open(convo_history_file_path, "wb") as f:
         pickle.dump(convo_history, f)
 
-
+@weave.op()
 def load_convo():
     """Load convo_history from a pickled file"""
     global convo_history
@@ -23,7 +24,7 @@ def load_convo():
     except FileNotFoundError:
         return []
 
-
+@weave.op()
 def get_convo_history_as_groq():
     """Get the conversation history as Groq-compatible messages."""
     return [
@@ -37,7 +38,7 @@ def get_convo_history_as_groq():
         and x["role"] in ["user", "assistant", "system", "bot"]
     ]
 
-
+@weave.op()
 def get_convo_history_as_vapi():
     """Get the conversation history as Vapi-compatible messages."""
     return [
